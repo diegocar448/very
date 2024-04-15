7d16e740dcf408426eabb5ce0b50750f0f17421f
Diego Cardoso <diegocar448@hotmail.com>
2024-04-15 00:06:45 -0300

    design do formulario

--- app/src/main/java/br/com/alura/aluvery/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/activities/ProductFormActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,7 +14,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.alura.aluvery.ui.theme.AluveryTheme
 
 class ProductFormActivity : ComponentActivity() {
@@ -34,13 +37,19 @@ class ProductFormActivity : ComponentActivity() {
 
 @Composable
 fun ProductFormScreen(){
-    Column {
-        Text(text = "Criando o produto")
+    Column(
+        Modifier.fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(text = "Criando o produto", Modifier.fillMaxWidth(), fontSize = 28.sp)
         var url by remember{
             mutableStateOf("")
         }
         TextField(value = url, onValueChange = {
             url = it
+        }, Modifier.fillMaxWidth(), label = {
+            Text(text = "Url da imagem")
         })
 
         var name by remember{
@@ -48,6 +57,8 @@ fun ProductFormScreen(){
         }
         TextField(value = name, onValueChange = {
             name = it
+        }, Modifier.fillMaxWidth(), label = {
+            Text(text = "Digite o nome")
         })
 
         var price by remember{
@@ -55,6 +66,8 @@ fun ProductFormScreen(){
         }
         TextField(value = price, onValueChange = {
             price = it
+        }, Modifier.fillMaxWidth(), label = {
+            Text(text = "Preço")
         })
 
         var description by remember{
@@ -62,9 +75,13 @@ fun ProductFormScreen(){
         }
         TextField(value = description, onValueChange = {
             description = it
+        }, Modifier.fillMaxWidth()
+            .heightIn(100.dp),
+            label = {
+            Text(text = "Descrição")
         })
         
-        Button(onClick = {}) {
+        Button(onClick = {}, Modifier.fillMaxWidth()) {
             Text(text="Salvar")
         }
     }
