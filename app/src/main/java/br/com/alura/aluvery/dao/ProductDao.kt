7e0ec58d4b9cce463d8f878335d7f903be095579
Diego Cardoso <diegocar448@hotmail.com>
2024-04-17 09:01:43 -0300

    ImplementandoDAOdosProdutos

---
+++ app/src/main/java/br/com/alura/aluvery/dao/ProductDao.kt
@@ -0,0 +1,12 @@
+package br.com.alura.aluvery.dao
+
+import br.com.alura.aluvery.sampledata.sampleProducts
+
+class ProductDao {
+
+    companion object{
+        private val products = sampleProducts.toMutableList()
+    }
+
+    fun products() = products.toList()
+}
