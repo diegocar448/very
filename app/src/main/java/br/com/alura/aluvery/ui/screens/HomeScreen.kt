c75db03c49098adbd9cb8094fe00a2c516b471c8
Diego Cardoso <diegocar448@hotmail.com>
2024-04-17 15:05:50 -0300

    Estados

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -16,45 +16,61 @@ import br.com.alura.aluvery.ui.components.ProductsSection
 import br.com.alura.aluvery.ui.components.SearchTextField
 import br.com.alura.aluvery.ui.theme.AluveryTheme
 
+class HomeScreenUiState(searchText: String) {
+    var text by mutableStateOf("")
+
+    val searchedProducts get() =
+        if (text.isNotBlank()) {
+            sampleProducts.filter { product ->
+                product.name.contains(
+                    text,
+                    ignoreCase = true,
+                ) ||
+                        product.description?.contains(
+                            text,
+                            ignoreCase = true,
+                        ) ?: false
+            }
+        } else emptyList()
+
+    fun isShowSection(): Boolean{
+        return text.isBlank()
+    }
+
+}
+
 @Composable
 fun HomeScreen(
     sections: Map<String, List<Product>>,
     searchText: String = ""
 ) {
     Column {
-        var text by remember {
-            mutableStateOf(searchText)
+
+        val state = remember {
+            HomeScreenUiState(searchText)
         }
+        val text = state.text
+        val searchedProducts = state.searchedProducts
+
+
         SearchTextField(
             searchText = text,
             onSearchChange = {
-                text = it
+                state.text = it
             },
+
             Modifier
                 .padding(16.dp)
                 .fillMaxWidth(),
         )
-        val searchedProducts = remember(text) {
-            if (text.isNotBlank()) {
-                sampleProducts.filter { product ->
-                    product.name.contains(
-                        text,
-                        ignoreCase = true,
-                    ) ||
-                            product.description?.contains(
-                                text,
-                                ignoreCase = true,
-                            ) ?: false
-                }
-            } else emptyList()
-        }
+
         LazyColumn(
             Modifier
                 .fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp),
             contentPadding = PaddingValues(bottom = 16.dp)
         ) {
-            if (text.isBlank()) {
+            if (state.isShowSection()) {
                 for (section in sections) {
                     val title = section.key
                     val products = section.value
